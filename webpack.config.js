5d1cd86877943c41ad7ac60d60e6832c25bebbbf
Umar Khan <ukhan@venacorp.com>
2019-07-28 23:29:46 -0400

    Add 'main' prop and change webpack bundle name

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js"
+    filename: "index.js"
   },
   module: {
     rules: [
