b04cbdd5e2323d3d0ae1b8a8afc662cfca7133d0
Umar Khan <ukhan@venacorp.com>
2019-07-29 22:22:58 -0400

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
 const path = require("path");
 
 module.exports = {
+  // mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-    library: "vena-test-component-library",
+    library: "vena-test-component-library-delete",
     libraryTarget: "umd"
   },
+  externals: [
+    {
+      react: {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom"
+      }
+    },
+    "@material-ui/core",
+    /@material-ui\/core\/*./
+  ],
   module: {
     rules: [
       { test: /\.(js)$/, use: "babel-loader" },
